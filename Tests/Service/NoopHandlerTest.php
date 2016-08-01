7b6514b754147fb9b4de3ec1c8b954dfde32a35c
Aerendir <hello@aerendir.me>
2016-08-01 11:32:03 +0200

    Test `NoopHandler`.

---
+++ Tests/Service/NoopHandlerTest.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of the AWS SES Monitor Bundle.
+ *
+ * @author Adamo Aerendir Crespi <hello@aerendir.me>
+ * @author Audrius Karabanovas <audrius@karabanovas.net>
+ */
+
+namespace SerendipityHQ\Bundle\AwsSesMonitorBundle\Tests\Service;
+
+use Aws\Credentials\Credentials;
+use SerendipityHQ\Bundle\AwsSesMonitorBundle\Service\HandlerFactory;
+use SerendipityHQ\Bundle\AwsSesMonitorBundle\Service\NoopHandler;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * @author Audrius Karabanovas <audrius@karabanovas.net>
+ * @author Adamo Aerendir Crespi <hello@aerendir.me>
+ *
+ * {@inheritdoc}
+ */
+class NoopHanlderTest extends \PHPUnit_Framework_TestCase
+{
+    public function testHandlerTest()
+    {
+        $mockRequest = $this->createMock(Request::class);
+        $mockCredentials = $this->createMock(Credentials::class);
+
+        $handler = new NoopHandler();
+
+        $this->assertSame(200, $handler->handleRequest($mockRequest, $mockCredentials));
+    }
+}
