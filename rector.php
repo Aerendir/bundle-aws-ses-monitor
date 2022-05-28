2467d3418f97e4ef14a62d736013e004c51f7700
Aerendir <hello@aerendir.me>
2022-05-28 17:01:26 +0200

    Disable Phan in `tests` Update Rector's config..

--- rector.php
+++ rector.php
@@ -11,20 +11,17 @@ declare(strict_types = 1);
  * file that was distributed with this source code.
  */
 
+use Rector\Config\RectorConfig;
 use SerendipityHQ\Integration\Rector\SerendipityHQ;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Rector\Core\Configuration\Option;
 use Rector\Core\ValueObject\PhpVersion;
 
-return static function (ContainerConfigurator $containerConfigurator) : void {
-    $parameters = $containerConfigurator->parameters();
+return static function (RectorConfig $rectorConfig) : void {
+    $rectorConfig->phpVersion(PhpVersion::PHP_74);
+    $rectorConfig->paths([__DIR__ . '/src', __DIR__ . '/tests']);
 
-    $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_74);
-    $parameters->set(Option::PATHS, [__DIR__ . '/src', __DIR__ . '/tests']);
-    $parameters->set(Option::BOOTSTRAP_FILES, [__DIR__ . '/vendor-bin/phpunit/vendor/autoload.php']);
-
-    $containerConfigurator->import(SerendipityHQ::SHQ_SYMFONY_BUNDLE);
+    $rectorConfig->bootstrapFiles([__DIR__ . '/vendor-bin/phpunit/vendor/autoload.php']);
+    $rectorConfig->import(SerendipityHQ::SHQ_SYMFONY_BUNDLE);
 
     $toSkip = SerendipityHQ::buildToSkip(SerendipityHQ::SHQ_SYMFONY_BUNDLE_SKIP);
-    $parameters->set(Option::SKIP, $toSkip);
+    $rectorConfig->skip($toSkip);
 };
